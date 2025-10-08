ec4de245126adbbc6fdc3629a232fa283b39d0e1
Wing Sun Au <142835730+WingSunAu@users.noreply.github.com>
2025-10-07 17:59:56 -0700

    Completed 94

---
+++ 94-Binary-Tree-Inorder-Traversal.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        // left -> middle -> right
+        vector<int> res;
+        helper(root, res);
+        return res;
+    }
+    void helper(TreeNode* curr, vector<int> &res){
+        if(curr == nullptr) return;
+        helper(curr->left, res);
+        res.push_back(curr->val);
+        helper(curr->right, res);
+    }
+};
